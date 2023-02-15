c9dfec7c41819d332d3e36900fdf964f12a7f7e0
gilles20393 <gilles1996@outlook.com>
2023-02-15 12:42:09 +0100

    oef 1 - start

--- labo/party/src/main/java/be/thomasmore/party/controllers/AboutController.java
+++ labo/party/src/main/java/be/thomasmore/party/controllers/AboutController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class AboutController {
 
-
-    @GetMapping({"/","/home"})
-    public String home(){
-        return "home";
+    @GetMapping("/about")
+    public String about(){
+        return "about";
     }
+
 }
