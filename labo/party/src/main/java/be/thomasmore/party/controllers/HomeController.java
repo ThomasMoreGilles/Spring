3e309c183e4f0a01dc4cd2367ad568ef29b4b8a5
gilles20393 <gilles1996@outlook.com>
2023-02-26 21:42:59 +0100

    data

--- labo/party/src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ labo/party/src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Controller
 public class HomeController {
-
     @GetMapping({"/", "/home"})
     public String home(Model model) {
         int myCalculatedValue = 34 * 62;
@@ -31,6 +31,12 @@ public class HomeController {
     @GetMapping("/pay")
     public String pay(Model model) {
         LocalDateTime now = LocalDateTime.now();
+        now = now.plusDays(5);
+        if (now.getDayOfWeek()==DayOfWeek.SATURDAY || now.getDayOfWeek()==DayOfWeek.SUNDAY) {
+            model.addAttribute("weekend", true);
+        } else {
+            model.addAttribute("weekend", false);
+        }
         DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         model.addAttribute("now", now.format(format));
         model.addAttribute("paydate", now.plusDays(30).format(format));
